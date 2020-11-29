b1f336421eb8396c8b45eb146479630b642fc628
Takahiro Abe <hirotaka081630@gmail.com>
2020-11-29 16:34:23 +0900

    Methods continued

--- Basics/04/02_methods-functions.go
+++ Basics/04/02_methods-functions.go
@@ -9,11 +9,11 @@ type vertex2 struct {
 	X, Y float64
 }
 
-func Abs(v vertex2) float64 {
+func abs(v vertex2) float64 {
 	return math.Sqrt(v.X*v.X + v.Y*v.Y)
 }
 
 func main() {
 	v := vertex2{3, 4}
-	fmt.Println(Abs(v))
+	fmt.Println(abs(v))
 }
